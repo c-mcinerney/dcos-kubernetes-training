2162b02141cae0668f4d63ec2a18728ece2cc44e
Chris McInerney <40616093+c-mcinerney@users.noreply.github.com>
2019-05-13 09:09:32 -0400

    Delete create-kubernetes-option-file.bat

--- windows/create-kubernetes-option-file.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SETLOCAL enableextensions enableDelayedExpansion
-
-set "formattedValue=000000%%1"
-@powershell -Command "get-content options-kubernetes-cluster.json.template | %% { $_ -replace \"TOBEREPLACED\",\"!formattedValue:~-2!\" }" > options-kubernetes-cluster!formattedValue:~-2!.json
